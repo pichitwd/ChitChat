86c10fa83dbb557d00cf29f0197545daeac2d34f
Pichit <pichit.wd@bru.ac.th>
2018-01-27 14:32:01 +0700

    Replace Fragment OK

---
+++ app/src/main/java/pichitwandee/th/ac/bru/chitchat/fragment/RegisterFragment.java
@@ -0,0 +1,25 @@
+package pichitwandee.th.ac.bru.chitchat.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pichitwandee.th.ac.bru.chitchat.R;
+
+/**
+ * Created by CS-BRU on 27/1/2561.
+ */
+
+public class RegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container,false);
+
+        return view;
+    }
+}
