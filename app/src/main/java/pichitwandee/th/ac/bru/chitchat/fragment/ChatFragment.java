822986cb0d6a7f29aa3be7399f99490deb309c95
Pichit <pichit.wd@bru.ac.th>
2018-01-28 14:05:29 +0700

    Drawer Menu Success

---
+++ app/src/main/java/pichitwandee/th/ac/bru/chitchat/fragment/ChatFragment.java
@@ -0,0 +1,27 @@
+package pichitwandee.th.ac.bru.chitchat.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pichitwandee.th.ac.bru.chitchat.R;
+
+/**
+ * Created by CS-BRU on 28/1/2561.
+ */
+
+public class ChatFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        return view;
+
+
+    }
+}
