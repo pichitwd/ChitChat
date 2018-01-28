d8c124a86e8aa405dcd010cda23cc5e90fb5afda
Pichit <pichit.wd@bru.ac.th>
2018-01-28 10:16:43 +0700

    Auth by email success

--- app/src/main/java/pichitwandee/th/ac/bru/chitchat/fragment/RegisterFragment.java
+++ app/src/main/java/pichitwandee/th/ac/bru/chitchat/fragment/RegisterFragment.java
@@ -1,6 +1,7 @@
 package pichitwandee.th.ac.bru.chitchat.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 import pichitwandee.th.ac.bru.chitchat.MainActivity;
 import pichitwandee.th.ac.bru.chitchat.R;
@@ -96,8 +104,34 @@ public class RegisterFragment extends Fragment {
         } else {
 
 //            No Space
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuth.createUserWithEmailAndPassword(emailString, passwordString)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
 
-        }
+                            if (task.isSuccessful()) {
+
+                                Toast.makeText(getActivity(), "Register success",
+                                        Toast.LENGTH_SHORT).show();
+                                getActivity().getSupportFragmentManager().popBackStack();
+                                //back to first page
+
+                            } else {
+
+                                MyAlert myAlert = new MyAlert(getActivity());
+                                myAlert.normalDialog("Cannot Register",
+                                        task.getException().getMessage());
+
+                            }
+
+
+                        } // onComplete
+                    });
+
+
+
+        }  // if
 
 
     }
