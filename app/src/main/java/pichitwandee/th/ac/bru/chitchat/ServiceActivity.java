361f96ad12712569ca78d9648519ee5a6f1ecd30
Pichit <pichit.wd@bru.ac.th>
2018-01-28 14:18:27 +0700

    Add ChatFragment Success

--- app/src/main/java/pichitwandee/th/ac/bru/chitchat/ServiceActivity.java
+++ app/src/main/java/pichitwandee/th/ac/bru/chitchat/ServiceActivity.java
@@ -3,12 +3,23 @@ package pichitwandee.th.ac.bru.chitchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import pichitwandee.th.ac.bru.chitchat.fragment.ChatFragment;
+import pichitwandee.th.ac.bru.chitchat.fragment.RegisterFragment;
+
 public class ServiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentServiceFragment, new ChatFragment())
+                    .commit();
+        }
+
     } //Main Method
 
 } //Main Class
